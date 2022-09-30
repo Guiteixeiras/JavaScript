e7cfdfff5a4992c7e17200a90f432089796be0d5
Hadson Martins <guilherme.tsilva03@gmail.com>
2022-09-30 16:50:52 -0400

    Update condições.js

--- condições.js
+++ condições.js
@@ -1,1 +1,17 @@
 var vel = 70.5
+var pais = 'EUA'
+
+console.log (`sua velocidade agora é de ${vel}km/h, acho melhor andar certo em`)
+
+if (vel >=65) {
+  console.log (`você esta agora a ${vel}km/h e foi MULTADO por isso`)
+}
+console.log (`lembre sempre do cinto de segurança`)
+
+
+console.log (`vivendo no ${pais}`)
+  if (pais =='Brasil') {
+    console.log (`você é brasileiro`)
+  }else{
+    console.log (`você é estrangeiro`)
+  }
