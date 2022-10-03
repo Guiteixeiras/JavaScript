8f6c9fcce18447b0af30315b87a78b0cd38e8e81
Guilherme <114297679+Guiteixeiras@users.noreply.github.com>
2022-10-03 17:34:41 -0400

    Add files via upload

---
+++ condiçõções_alinhadas.js
@@ -0,0 +1,38 @@
+
+  var idade = 65
+  if (idade < 16) {
+    console.log ('Não vota')
+  }else {
+    if (idade < 18) {
+      console.log ('Voto opcional') 
+      }else {
+          if (idade < 65) {
+            console.log ('Voto obrigatorio')
+          }else{
+              if (idade < 200) {
+            console.log('Voto opcional')
+          }
+        }
+      }
+  }
+
+  var hora = 8
+  console.log(`Agora são ${hora} horas`)
+  if (hora < 12) {
+    console.log('bom dia')
+  }else if (hora <= 18){
+    console.log('boa tarde')
+  }else {
+    console.log('boa noite')
+  }
+
+  var agora = new Date()
+  var hora = agora.getHours()
+  console.log(`Agora são ${hora} horas`)
+  if (hora < 12) {
+    console.log('bom dia')
+  }else if (hora <= 18){
+    console.log('boa tarde')
+  }else {
+    console.log('boa noite')
+  }
