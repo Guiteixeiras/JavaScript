9e09283b9dd7fd9bf00b4fa96df0c5e0d99cacf0
murilomafra1302 <murilommafra@gmail.com>
2022-10-06 16:58:36 -0400

    verificador de idade

--- atv2/script.js
+++ atv2/script.js
@@ -1,13 +1,58 @@
 function verificar() {
-  var data = new Data()
-  var ano = data.getFukkYear()
-  var fao = document.getElementById('txtano')
-  var res = document.getElementById('res')
-  if (fano.value.length == 0 || Number(fano.value) > ano) {
-    window.alert('Verifique se está tudo realmente certo!!!!!')
-  } else {
-    var fsex = document.getElementsByName('radsex')
-    var idade = ano - Number(fano.value)
-    res.innerHTML =`Iade calculada ${idade}`
+    //window.alert('funciona')
+    
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano').value;
+    var res = document.querySelector('div#res')
+    console.log(fano);
+    if (fano == 0 || fano > ano) {
+      window.alert('Verifique se está tudo realmente certo!!!!!')
+    } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - fano
+      var genero = ''
+      var img = document.createElement ('img')
+      img.setAttribute ('id',  'foto')
+      if(fsex[0].checked) {
+        genero = 'homem'
+        if (idade >=0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'imagens/Ocriança.png')
+        }else if (idade <21 ){
+           //adolescente
+           img.setAttribute('src', 'imagens/Ojovem.png')
+        }else if (idade <50 ){
+          //adulto
+          img.setAttribute('src', 'imagens/adulto.png')
+        }else{
+         //idoso
+         img.setAttribute('src', 'imagens/idoso.png')
+        }
+      }else if(fsex[1].checked){
+        genero = 'mulher'
+        if (idade >=0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'imagens/Acriança.png')
+        }else if (idade <21 ){
+           //adolescente
+           img.setAttribute('src', 'imagens/Ajovem.png')
+        }else if (idade <50 ){
+          //adulto
+          img.setAttribute('src', 'imagens/adulta.png')
+        }else{
+         //idoso
+         img.setAttribute('src', 'imagens/idosa.png')
+        }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML =`detectamos ${genero} com ${idade} anos de idade`
+      res.appendChild(img)
+    }
   }
-}
+ 
+  var button = document.getElementById('button');
+  button.addEventListener('click',()=>{
+    verificar();
+    
+  })
