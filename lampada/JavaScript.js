57acbcda173c04ddbcce4a1f052c77a401b198c8
Guilherme <114297679+Guiteixeiras@users.noreply.github.com>
2022-10-06 15:21:05 -0400

    lampada

---
+++ lampada/JavaScript.js
@@ -0,0 +1,25 @@
+const turnOn = document.getElementById ( 'turnOn' );
+const turnOff = document.getElementById ( 'turnOff' );
+const lamp = document.getElementById ( 'lamp' );
+
+function isLampBroken() {
+    return lamp.src.indexOf ('quebrada') > -1
+}
+
+function lampOn () {
+  if ( !isLampBroken ()) {
+    lamp.src = './imagens/acessa.png';
+  }
+}
+function lampOff () {
+  if ( !isLampBroken ()) {
+    lamp.src = './imagens/apagada.png';
+  }
+}
+function lampBroken () {
+    lamp.src = './imagens/quebrada.png';
+}
+
+turnOn.addEventListener ('click', lampOn);
+turnOff.addEventListener ('click', lampOff);
+lamp.addEventListener ('dblclick', lampBroken);
